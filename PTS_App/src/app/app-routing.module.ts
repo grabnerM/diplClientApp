d01799272e24d9c54efbe35794d33359ea3a5b53
grabnerM <56301152+grabnerM@users.noreply.github.com>
2020-10-26 23:28:49 +0100

    Error 'cant bind ngIf...' fixed

---
+++ PTS_App/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- PTS_App/src/app/app.module.ts
+++ PTS_App/src/app/app.module.ts
@@ -6,31 +6,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { CommonModule } from '@angular/common';
-
-import { IonicStorageModule } from '@ionic/storage';
-import { HttpClientModule } from '@angular/common/http';
-import { AuthenticationComponent } from './components/authentication/authentication.component';
-import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent, 
-    AuthenticationComponent
-  ],
-  entryComponents: [
-    AuthenticationComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
+  entryComponents: [HomeComponent],
   imports: [
     BrowserModule, 
     CommonModule,
-    FormsModule,
-    IonicModule.forRoot(),
-    AppRoutingModule,
-    HttpClientModule,
-    IonicStorageModule.forRoot(),
+    IonicModule.forRoot(), 
+    AppRoutingModule
   ],
   providers: [
     StatusBar,
